c1b60e8c7baefc70bc851150a6aaf40c45c23311
Jan Klass <kissaki@posteo.de>
2022-06-05 18:55:09 +0200

    test: Fix tests after introduction of BsonException

--- Kcode.BSON.Tests/BSONSerializerTest.cs
+++ Kcode.BSON.Tests/BSONSerializerTest.cs
@@ -14,27 +14,27 @@ public class BSONSerializerTest
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1825:Avoid zero-length array allocations", Justification = "Uniform test case code")]
     public void Test_SmallerMinBSON_Throws()
     {
-        Assert.Throws<InvalidOperationException>(() => BsonSerializer.DeserializeDocument(new byte[] { }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, 0x00, }));
     }
 
     [Fact]
     public void Test_SmallerMinLen_Throws()
     {
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x02, 0x00, 0x00, 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x03, 0x00, 0x00, 0x00, 0x00, }));
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x04, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x02, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x03, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0x04, 0x00, 0x00, 0x00, 0x00, }));
     }
 
     [Fact]
     public void Test_NegLen_Throws()
     {
-        Assert.Throws<FormatException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0b100, 0x00, 0x00, 0x00, 0x00, }));
+        Assert.Throws<BsonException>(() => BsonSerializer.DeserializeDocument(new byte[] { 0b100, 0x00, 0x00, 0x00, 0x00, }));
     }
 
     private static IEnumerable<byte> ParseHexBytes(string hexBytes)
