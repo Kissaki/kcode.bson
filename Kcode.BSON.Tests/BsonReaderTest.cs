c53236b82af8f2ac97a1724d2ccc33f4633845fc
Jan Klass <kissaki@posteo.de>
2023-10-14 18:01:54 +0200

    test: Fix Test_CString

--- Kcode.BSON.Tests/BsonReaderTest.cs
+++ Kcode.BSON.Tests/BsonReaderTest.cs
@@ -8,19 +8,22 @@ public class BsonReaderTest
     public void Test_CString()
     {
         Assert.Equal("", BsonReader.ReadCString(BinR("00")));
+        Assert.Equal("a", BsonReader.ReadCString(BinR("6100")));
+        Assert.Equal("A", BsonReader.ReadCString(BinR("4100")));
+        Assert.Equal("1259", BsonReader.ReadCString(BinR("3132353900")));
     }
 
-    private BinaryReader BinR(string hex)
+    private static BinaryReader BinR(string hex)
     {
         return new BinaryReader(new MemoryStream(Bin(hex)));
     }
 
-    private byte[] Bin(string hex)
+    private static byte[] Bin(string hex)
     {
         var b = new byte[hex.Length / 2];
         for (var i = 0; i < hex.Length; i += 2)
         {
-            b[i / 2] = byte.Parse(hex[i..(i + 2)]);
+            b[i / 2] = Convert.ToByte(hex.Substring(i, 2), fromBase: 16);
         }
         return b;
     }
